0b72bd29ef904e84f393c172a6ac35f74f342022
André Carmo <andre_de11@msn.com>
2021-06-30 12:41:32 -0300

    Sign in backend behavior and tests.

--- tests/fashionCamp.test.js
+++ tests/fashionCamp.test.js
@@ -28,7 +28,7 @@ describe("POST /fashioncamp/sign-up", () => {
         expect(response.status).toEqual(201);
     });
 
-    it("returns status 400 for invalid params on body", async () => {
+    it("returns status 400 for invalid params", async () => {
         const body = {
             name: "",
             email: "teste@teste.com",
@@ -59,13 +59,76 @@ describe("POST /fashioncamp/sign-up", () => {
         };
 
         await connection.query(
-            `INSERT INTO users (name, email, password) VALUES ('Testeiro2', $1, '789451')`,
-            [body.email]
+            `INSERT INTO users (name, email, password) VALUES ($1, $2, $3)`,
+            ["Testeiro2", body.email, "756342"]
         );
 
         const response = await supertest(app)
             .post("/fashioncamp/sign-up")
             .send(body);
         expect(response.status).toEqual(409);
+    });
+});
+
+describe("POST /fashioncamp/sign-in", () => {
+    beforeEach(async () => {
+        await connection.query("DELETE FROM users");
+        await connection.query("DELETE FROM sessions");
+    });
+
+    it("returns status 200 for valid params", async () => {
+        const body = {
+            email: "teste@teste.com",
+            password: "654321",
+        };
+
+        await supertest(app)
+            .post("/fashioncamp/sign-up")
+            .send({ ...body, name: "Testeiro" });
+
+        const response = await supertest(app)
+            .post("/fashioncamp/sign-in")
+            .send(body);
+        expect(response.status).toEqual(200);
+        expect(response.body).toEqual(
+            expect.objectContaining({
+                name: expect.any(String),
+                token: expect.any(String),
+            })
+        );
+    });
+
+    it("returns status 400 for invalid params", async () => {
+        const body = {
+            email: "teste@teste.com",
+            password: "654321",
+        };
+
+        await connection.query(
+            `INSERT INTO users (name, email, password) VALUES ($1, $2, $3)`,
+            ["Testeiro", body.email, body.password]
+        );
+
+        const firstTry = await supertest(app)
+            .post("/fashioncamp/sign-in")
+            .send({ ...body, email: "teste" });
+        expect(firstTry.status).toEqual(400);
+
+        const secondTry = await supertest(app)
+            .post("/fashioncamp/sign-in")
+            .send({ ...body, password: "" });
+        expect(secondTry.status).toEqual(400);
+    });
+
+    it("returns status 404 for user not found", async () => {
+        const body = {
+            email: "teste@teste.com",
+            password: "654321",
+        };
+
+        const response = await supertest(app)
+            .post("/fashioncamp/sign-in")
+            .send(body);
+        expect(response.status).toEqual(404);
     });
 });
