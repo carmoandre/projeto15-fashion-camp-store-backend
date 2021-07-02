d4780c86963baf6ce32573752e35c8a591b4d3e9
André Carmo <andre_de11@msn.com>
2021-07-02 17:15:36 -0300

    Backend behavior of cart routes.

--- src/app.js
+++ src/app.js
@@ -102,13 +102,142 @@ app.post("/fashioncamp/sign-in", async (req, res) => {
 /* Show Products Route */
 
 /* Cart Routes */
-app.get("/fashioncamp/cart/", async (req, res) => {});
+app.get("/fashioncamp/cart/", async (req, res) => {
+    const authorization = req.header("Authorization");
+    const token = authorization?.replace("Bearer ", "");
 
-app.put("/fashioncamp/cart/alter-product-quantity", async (req, res) => {});
+    if (!token) return res.sendStatus(401);
+
+    const result = await connection.query(
+        `SELECT * FROM sessions
+        WHERE token=$1`,
+        [token]
+    );
+
+    const userId = result.rows.length ? result.rows[0].userId : null;
+    if (!userId) return res.status(404).send("Usuário não encontrado");
+
+    try {
+        const activeCart = await connection.query(
+            `SELECT * FROM cart WHERE "userId"=$1 AND "isActive"=TRUE`,
+            [userId]
+        );
+        const cart = activeCart.rows.length
+            ? activeCart.rows[0]
+            : (
+                  await connection.query(
+                      `INSERT INTO carts ("userId", "isActive")
+                      VALUES ($1, $2) RETURNING *`,
+                      [userId, "TRUE"]
+                  )
+              ).rows[0];
+        const cartProducts = await connection.query(
+            `SELECT products.id, products.name, 
+            products.value, prodcuts.quantity AS stock, 
+            cartproducts.quantity, product.image
+            FROM cartproducts JOIN products
+            ON cartproducts."productId"=products.id 
+            WHERE "cartId"=$1 AND "isActive"=TRUE`,
+            [cart.id]
+        );
+        res.status(200).send({
+            cartid: cart.id,
+            cartProducts: cartProducts.rows,
+        });
+    } catch (err) {
+        console.log(err);
+        res.sendStatus(400);
+    }
+});
+
+app.put("/fashioncamp/cart/alter-product-quantity", async (req, res) => {
+    const authorization = req.header("Authorization");
+    const token = authorization?.replace("Bearer ", "");
+
+    if (!token) return res.sendStatus(401);
+
+    const result = await connection.query(
+        `SELECT * FROM sessions
+        WHERE token=$1`,
+        [token]
+    );
+
+    const userId = result.rows.length ? result.rows[0].userId : null;
+    if (!userId) return res.status(404).send("Usuário não encontrado");
+
+    const validation = customersSchema.validate(req.body);
+    if (validation.error) {
+        res.sendStatus(400);
+        return;
+    }
+
+    const { cartId, productId, quantity } = req.body;
+    const existentCart = await connection.query(
+        `SELECT * FROM carts WHERE id=$1 AND "isActive"=TRUE`,
+        [cartId]
+    );
+
+    if (existentCart.rows.length) {
+        res.sendStatus(409);
+        return;
+    }
+    try {
+        await connection.query(
+            `UPDATE cartproducts SET quantity=$1 WHERE "cartId"=$2 AND "productId"=$3`,
+            [quantity, cartId, productId]
+        );
+        res.sendStatus(200);
+    } catch (err) {
+        console.log(err);
+        res.sendStatus(400);
+    }
+});
 
 app.delete(
     "/fashioncamp/cart/remove-product/:cartId/:productId",
-    async (req, res) => {}
+    async (req, res) => {
+        const authorization = req.header("Authorization");
+        const token = authorization?.replace("Bearer ", "");
+
+        if (!token) return res.sendStatus(401);
+
+        const result = await connection.query(
+            `SELECT * FROM sessions
+        WHERE token=$1`,
+            [token]
+        );
+        const userId = result.rows.length ? result.rows[0].userId : null;
+        const { cartId, productId } = req.params;
+        const existentCart = await connection.query(
+            `SELECT * FROM carts WHERE id=$1`,
+            [cartId]
+        );
+        const existentProduct = await connection.query(
+            `SELECT * FROM products WHERE id=$1`,
+            [productId]
+        );
+        if (
+            !userId ||
+            !existentCart.rows.length ||
+            !existentProduct.rows.length
+        ) {
+            res.sendStatus(404);
+            return;
+        }
+
+        try {
+            connection.query(
+                `DELETE FROM cartproducts WHERE "cartId"=$1 ANDND "productId"=$2`,
+                [cartId, productId]
+            );
+            res.sendStatus(200);
+        } catch (err) {
+            console.log(err);
+            res.status(400).send(
+                "Erro ao tentar excluir o produto do carrinho"
+            );
+        }
+    }
 );
 
 export default app;
